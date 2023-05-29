cddcdcac58788e19ba9fa6251a9dbd8a7f5c8092
catalms <catilms13@gmail.com>
2023-05-29 14:31:03 +0100

    insertions added

--- populate.sql
+++ populate.sql
@@ -8,3 +8,80 @@ INSERT INTO customer VALUES (7, 'Liam Anderson', 'liamanderson@mail', '777999888
 INSERT INTO customer VALUES (8, 'Olivia Taylor', 'oliviataylor@mail', '1112223333', '741 Willow Ave');
 INSERT INTO customer VALUES (9, 'James Thomas', 'jamesthomas@mail', '4447771111', '963 Spruce St');
 INSERT INTO customer VALUES (10, 'Isabella Clark', 'isabellaclark@mail', '9998887777', '258 Oakwood Dr');
+
+INSERT INTO orders VALUES (1, '2023-05-01');
+INSERT INTO orders VALUES (2, '2023-05-02');
+INSERT INTO orders VALUES (3, '2023-05-03');
+INSERT INTO orders VALUES (4, '2023-05-04');
+INSERT INTO orders VALUES (5, '2023-05-05');
+INSERT INTO orders VALUES (6, '2023-05-06');
+INSERT INTO orders VALUES (7, '2023-05-07');
+INSERT INTO orders VALUES (8, '2023-05-08');
+INSERT INTO orders VALUES (9, '2023-05-09');
+INSERT INTO orders VALUES (10, '2023-05-10');
+
+INSERT INTO places VALUES (1, 5);
+INSERT INTO places VALUES (2, 3);
+INSERT INTO places VALUES (3, 4);
+INSERT INTO places VALUES (4, 3);
+INSERT INTO places VALUES (5, 9);
+
+INSERT INTO sale VALUES (1);
+INSERT INTO sale VALUES (2);
+INSERT INTO sale VALUES (3);
+INSERT INTO sale VALUES (4);
+INSERT INTO sale VALUES (5);
+
+INSERT INTO pay VALUES (1, 4);
+INSERT INTO pay VALUES (2, 1);
+INSERT INTO pay VALUES (3, 7);
+INSERT INTO pay VALUES (4, 2);
+INSERT INTO pay VALUES (5, 3);
+
+INSERT INTO employee VALUES (1, 1111111, '1990-01-01', 'John Snow');
+INSERT INTO employee VALUES (2, 2222222, '1993-04-22', 'Alice White');
+INSERT INTO employee VALUES (3, 3333333, '1990-07-03', 'Michael Black');
+INSERT INTO employee VALUES (4, 4444444, '1994-10-14', 'Emily Green');
+INSERT INTO employee VALUES (5, 5555555, '1991-01-25', 'Daniel Brown');
+INSERT INTO employee VALUES (6, 6666666, '1992-04-06', 'Sophia Gray');
+INSERT INTO employee VALUES (7, 7777777, '1995-07-17', 'Liam Taylor');
+INSERT INTO employee VALUES (8, 8888888, '1993-10-28', 'Olivia Anderson');
+INSERT INTO employee VALUES (9, 9999999, '1996-01-08', 'James Martinez');
+INSERT INTO employee VALUES (10, 1010101, '1994-04-19', 'Isabella Clark');
+
+INSERT INTO process VALUES (1, 1);
+INSERT INTO process VALUES (2, 2);
+INSERT INTO process VALUES (3, 3);
+INSERT INTO process VALUES (4, 4);
+INSERT INTO process VALUES (5, 5);
+
+INSERT INTO department VALUES ('Sales');
+INSERT INTO department VALUES ('Marketing');
+INSERT INTO department VALUES ('Human Resources');
+INSERT INTO department VALUES ('Research and Development');
+INSERT INTO department VALUES ('Accounting');
+
+INSERT INTO workplace VALUES (1, '676 Elm St');
+INSERT INTO workplace VALUES (2, '144 Main St');
+INSERT INTO workplace VALUES (3, '123 Oak Ave');
+INSERT INTO workplace VALUES (4, '987 Maple Rd');
+INSERT INTO workplace VALUES (5, '6 Pine Ln');
+
+INSERT INTO works VALUES (1, 'Human Resources', '144 Main St');
+INSERT INTO works VALUES (2, 'Sales', '676 Elm St');
+INSERT INTO works VALUES (3, 'Sales', '123 Oak Ave');
+INSERT INTO works VALUES (4, 'Marketing', '987 Maple Rd');
+INSERT INTO works VALUES (5, 'Research and Development', '987 Oak Ave');
+INSERT INTO works VALUES (6, 'Accounting', '6 Pine Ln');
+
+INSERT INTO office VALUES ('144 Main St');
+INSERT INTO office VALUES ('676 Elm St');
+INSERT INTO office VALUES ('123 Oak Ave');
+INSERT INTO office VALUES ('987 Maple Rd');
+INSERT INTO office VALUES ('6 Pine Ln');
+
+INSERT INTO warehouse VALUES ('123 Oak Ave');
+INSERT INTO warehouse VALUES ('987 Maple Rd');
+INSERT INTO warehouse VALUES ('6 Pine Ln');
+
+INSERT INTO product VALUES (1, 'Water Gun', 'gun that shoots out water',5.99);
